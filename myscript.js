244bb61eda8fe9b26bc3a31dfe654655a65cf486
Nipun Nirmana <nipunfernando@hotmail.com>
2013-02-11 18:36:52 +0530

    Init Commit

---
+++ myscript.js
@@ -0,0 +1,14 @@
+//$(document).ready( function (){
+//    $(".uiButton").click( function (){
+//        $(this).css("backgroundColor","grey");
+//    } );
+//    
+//     $(":not(.uiButton)").click( function (){
+//        alert('this');
+//    } );
+//    
+//    
+//} );
+
+//document.write(chrome.extension.getURL ("blueBar_bg.png") );
+
